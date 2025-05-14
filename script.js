1ea21af6378498831ad9c1ae3872f6298ad3163a
carlaFF <carlafalogme@gmail.com>
2025-05-14 17:27:33 +0800

    Implemented font size change on selection

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ fontSelector.addEventListener("change", function(event) {
     textElement.style.fontFamily=selectedFont;
     console.log(selectedFont);
 });
+
+const sizeSelector = document.getElementById("size-selector");
+
+sizeSelector.addEventListener("change", function(event) {
+    const selectedSize = event.target.value;
+    const textElement = document.getElementById("text");
+    textElement.style.fontSize=selectedSize + "px";
+    console.log(selectedSize);
+});
